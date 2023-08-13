cb43c880c4a38de2138d8c2da2cea0f7a3674349
HamJohni <joni.pacan.wf@gmail.com>
2023-08-13 19:53:06 +0600

    all have done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Suspense } from 'react';
+import PrivatRounting from './routing/PrivatRounting';
+import AuthRouting from './routing/AuthRouting';
+import '../src/styles/style.scss';
+
+
 
 function App() {
 
-  return (
-    <div className="App">
 
-    </div>
+
+  return (
+    <Suspense fallback={'...loading'}>
+        <AuthRouting/>
+
+        <PrivatRounting/>
+    </Suspense>
   );
 }
 
